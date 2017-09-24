0718b0f9b43172d520a66c81c0d695a6c36170d7
Elad Keyshawn <elad.keyshawn@gmail.com>
2017-09-24 22:14:57 +0300

    auto create app/ and routing subdirs

--- endpoint.js
+++ endpoint.js
@@ -1,46 +1,82 @@
 let path = require('path');
 const fs = require('fs');
-module.exports = function() {
+const _ = require('lodash');
 
+function setupRootRequire() {
     global.rootRequire = function(name) {
         // escape this dir and node_modules/
         console.log(path.normalize(`${__dirname}/../../${name}`));
         try {
-           return require(path.normalize(`${__dirname}/../../${name}`));
+            return require(path.normalize(`${__dirname}/../../${name}`));
         } catch (err) {
             console.log("rootRequire error: file could not be found ");
-           return null;
+            return null;
         }
     };
-    let testingRouterPath = './Router.js';
-    let routerPath = path.normalize(`${__dirname}/../../Router.js`);
+}
 
-    console.log("Processing Router.js ", testingRouterPath);
-    if (!fs.existsSync(testingRouterPath)) {
+function processRouterFile(routerPath) {
+
+
+    console.log("generating app routing...");
+    if(!fs.existsSync(routerPath + 'app')) {
+        fs.mkdirSync(routerPath + 'app');
+        console.log(routerPath + 'app', "created.");
+
+    }
+    if(!fs.existsSync(routerPath + 'app/controllers')) {
+        fs.mkdirSync(routerPath + 'app/controllers');
+        console.log(routerPath + 'app/controllers', "created.");
+
+    }
+    if(!fs.existsSync(routerPath + 'app/middleware')) {
+        fs.mkdirSync(routerPath + 'app/middleware');
+        console.log(routerPath + 'app/middleware', "created.");
+
+    }
+    if(!fs.existsSync(routerPath + 'app/routes')) {
+        fs.mkdirSync(routerPath + 'app/routes');
+        console.log(routerPath + 'app/routes', "created.");
+
+    }
+    const routerFilePath = routerPath + 'app/Router.js';
+    console.log("Processing Router.js ", routerFilePath);
+    if (!fs.existsSync(routerFilePath)) {
         console.log("No Route.js was found!");
 
+        fs.openSync(routerFilePath,'a');
+        console.log("Router.js created!");
+        const RouterStarter = fs.readFileSync('./test/RouterStarter.txt');
 
-        fs.open(testingRouterPath,'a', function (err) {
-            if (err) {
-                console.error(err);
-                console.error("endpoint.js: could not create Router.js file");
-                return;
-            }
-                console.log("Router.js created!");
-            const RouterStarter = fs.readFileSync('./test/RouterStarter.txt');
+        fs.writeFileSync(routerFilePath,RouterStarter);
 
-            fs.writeFile(testingRouterPath,RouterStarter, function (err) {
-                if(!err) {
-                    console.log("Router.js boilerplate ready!");
-                }
-            })
-        });
+        console.log("Router.js boilerplate ready!");
+        return null;
+    } else {
+        return require(routerFilePath);
+    }
+}
+
+module.exports = function(app,{prefix,test}){
+
+    setupRootRequire();
+
+    if(!prefix) prefix = "";
+    let routerPath = path.normalize(`${__dirname}/../../`);
+
+    if(test) {
+        routerPath = './';
+        console.log('testing mode, routing folder', routerPath);
+
     }
 
-    // let Router = rootRequire('app/Router');
-    // Router.forEach((route) => {
-    //     layPathStructure("/api",route);
-    // });
+    const Router = processRouterFile(routerPath);
+
+    if(!Router) return;
+
+    Router.forEach((route) => {
+        layPathStructure(prefix,route);
+    });
 
     function layPathStructure(prefix, route){
         if(_.has(route, 'subpaths')) {
--- test/test.js
+++ test/test.js
@@ -1,1 +1,12 @@
-require('../endpoint')();
+"use strict";
+
+const express = require('express');
+const http = require('http');
+let app = express();
+require('../endpoint')(app,{prefix:"", test:true});
+
+
+http.createServer(app).listen(4321, function() {
+    console.log('test: listening now to port 4321\n');
+
+});
